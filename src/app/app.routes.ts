8220324c9352626f278f87ed630f1d0ac5715140
yingtung99 <maisy8799@gmail.com>
2026-05-27 21:24:24 +0800

    [add] 基本架構建立

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserHome } from './modules/user-home/user-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: UserHome,
+  },
+];
