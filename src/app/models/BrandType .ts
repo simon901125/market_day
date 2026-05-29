f189928a163110d7bf5bad72fd469fca79525443
yingtung99 <maisy8799@gmail.com>
2026-05-29 16:02:34 +0800

    [add] 統一品牌類別和狀態類別

---
+++ src/app/models/BrandType .ts
@@ -0,0 +1,24 @@
+export class BrandType {
+  static readonly food = '餐飲美食';
+  static readonly handmade = '文創手作';
+  static readonly family = '親子家庭';
+  static readonly pet = '寵物生活';
+  static readonly plant = '植物選物';
+  static readonly fashion = '服飾配件';
+  static readonly toy = '玩具選物';
+
+  static readonly list: string[] = [
+    BrandType.food,
+    BrandType.handmade,
+    BrandType.family,
+    BrandType.pet,
+    BrandType.plant,
+    BrandType.fashion,
+    BrandType.toy,
+  ];
+
+  static readonly filterList: string[] = [
+    '全部市集',
+    ...BrandType.list,
+  ];
+}
