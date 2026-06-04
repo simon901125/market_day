c475c2b1b12169e3c6fb0ddf4211a4d67b75fca3
lin80092091 <jenny80092@gmail.com>
2026-06-04 22:40:45 +0800

    [add]新增css root: 圓角設定

--- src/app/modules/user/user-brand-detail/user-brand-detail.ts
+++ src/app/modules/user/user-brand-detail/user-brand-detail.ts
@@ -7,11 +7,14 @@ interface ProductItem {
   image: string;
   name: string;
   price: number;
+  description: string;
 }
 
 interface MarketRecord {
   name: string;
-  dateRange: string;
+  year: string;
+  startDate:string;
+  endDate:string;
 }
 
 interface CurrentMarket {
@@ -32,15 +35,15 @@ export class UserBrandDetail {
   readonly defaultImage = 'assets/images/PromotionalPhotos_2.png';
 
   mockProducts: ProductItem[] = [
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '蜂蜜蛋糕禮盒', price: 380 },
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '手工瑪德蓮', price: 240 },
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '低糖布丁組', price: 290 },
+    { image: 'assets/images/PromotionalPhotos_2.png', name: '蜂蜜蛋糕禮盒', price: 380, description:'美味好吃的東西' },
+    { image: 'assets/images/PromotionalPhotos_2.png', name: '手工瑪德蓮', price: 240, description:'美味好吃的東西' },
+    { image: 'assets/images/PromotionalPhotos_2.png', name: '低糖布丁組', price: 290, description:'美味好吃的東西' },
   ];
 
   mockMarketRecords: MarketRecord[] = [
-    { name: '草悟野餐市集', dateRange: '2025/03/15 ~ 2025/03/16' },
-    { name: '咖啡生活節', dateRange: '2025/05/10 ~ 2025/05/11' },
-    { name: '夏日風格服裝市集', dateRange: '2025/07/05 ~ 2025/07/06' },
+    { name: '草悟野餐市集', year: '2025', startDate: '03/27', endDate: '03/30' },
+    { name: '咖啡生活節', year: '2025', startDate: '05/10', endDate: '05/11' },
+    { name: '夏日風格服裝市集', year: '2025', startDate: '07/02', endDate: '07/05' },
   ];
 
   mockCurrentMarket: CurrentMarket = {
