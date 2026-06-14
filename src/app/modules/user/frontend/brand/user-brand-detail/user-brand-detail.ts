d9520a24bd00bde8b3c3c7f512fdceff67f913df
yingtung99 <maisy8799@gmail.com>
2026-06-14 16:33:41 +0800

    [update] 一般使用者關於我們樣式和資料夾圖片整理

--- src/app/modules/user/frontend/brand/user-brand-detail/user-brand-detail.ts
+++ src/app/modules/user/frontend/brand/user-brand-detail/user-brand-detail.ts
@@ -32,12 +32,12 @@ interface contactLinks{
 export class UserBrandDetail {
   brand: BrandItem | null = null;
 
-  readonly defaultImage = 'assets/images/PromotionalPhotos_2.png';
+  readonly defaultImage = 'assets/images/user/brand/promotional-photo-02.png';
 
   mockProducts: ProductItem[] = [
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '蜂蜜蛋糕禮盒', price: 380, description:'美味好吃的東西' },
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '手工瑪德蓮', price: 240, description:'美味好吃的東西' },
-    { image: 'assets/images/PromotionalPhotos_2.png', name: '低糖布丁組', price: 290, description:'美味好吃的東西' },
+    { image: 'assets/images/user/brand/promotional-photo-02.png', name: '蜂蜜蛋糕禮盒', price: 380, description:'美味好吃的東西' },
+    { image: 'assets/images/user/brand/promotional-photo-02.png', name: '手工瑪德蓮', price: 240, description:'美味好吃的東西' },
+    { image: 'assets/images/user/brand/promotional-photo-02.png', name: '低糖布丁組', price: 290, description:'美味好吃的東西' },
   ];
 
   mockMarketRecords: MarketRecord[] = [
