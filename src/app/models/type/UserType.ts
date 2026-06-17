a6056fe756b6c1a9fbc229fa0cd4801a943f052a
lin80092091 <jenny80092@gmail.com>
2026-06-17 22:46:01 +0800

    [add]新增UserType

---
+++ src/app/models/type/UserType.ts
@@ -0,0 +1,12 @@
+/** 使用者類型 */
+export class UserType {
+  /** 一般使用者 */
+  static readonly user = '一般使用者';
+  /** 攤主 */
+  static readonly vendor = '攤主';
+  /** 主辦方 */
+  static readonly organizer = '主辦方';
+  /** 管理員 */
+  static readonly admin = '管理員';
+
+}
