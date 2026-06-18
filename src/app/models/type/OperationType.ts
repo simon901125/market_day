aa5b87a289945e35fd95532debfc51c082591893
lin80092091 <jenny80092@gmail.com>
2026-06-18 14:51:58 +0800

    [add]新增活動狀態對應顏色與取得顏色class方法

---
+++ src/app/models/type/OperationType.ts
@@ -0,0 +1,13 @@
+/**管理員操作類型 */
+export class OperationType {
+    /** 活動審核 */
+    static readonly activityReview = '活動審核';
+    /** 要求補件 */
+    static readonly requestRevision = '要求補件';
+    /** 帳號恢復 */
+    static readonly accountRestored = '帳號恢復';
+    /** 帳號停用 */
+    static readonly accountDisabled = '帳號停用';
+    /**系統設定 */
+    static readonly systemSetting = '系統設定';
+}
